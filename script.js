18c61670494c54ffb953b2003ef4c1b72d64868c
Prodipta Nayeb <nayebprodipta@gmail.com>
2026-02-01 09:53:22 +0000

    added the entey of numbers in the calculator

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-let string=" ";
-let buttons= document.querySelectorAll(".keys");
-Array.from(buttons).forEach((keys)=>{
-    keys.addEventListener("click",(e)=>{
-        if(e.target) 
+let string = "";
+let buttons = document.querySelectorAll(".keys");
+Array.from(buttons).forEach((keys) => {
+    keys.addEventListener("click", (e) => {
+        if (e.target) 
         {
-            console.log(e.target.value);
-            str= str+ e.target.innerHTML;
-            document.querySelector("input").value= str;
+            val= e.target.innerHTML;
+            if(val== "=")
+                {
+                    string= eval(string);
+                    string= string+ val;
+                    document.querySelector("input").value= string;
+                }
+                else
+                {
+                    console.log(val);
+                    string= string + val;
+                    document.querySelector("input").value= string;
+                }
+            if(val!="DEL"&&val!="AC"&&val!="="&&val!="%")
+            {
+                console.log(val);
+                // if(val=="+")
+                // {
+                //     string = string + "+";
+                //     document.querySelector("input").value = string;
+                // }
+                string = string + val;
+                document.querySelector("input").value = string;
+            }
+            if(val=="AC")
+            {
+                string = "";
+                document.querySelector("input").value = string;
+            }
+            if(val=="DEL")
+            {
+                string = string.slice(0,-1);
+                document.querySelector("input").value = string;
+            }
+            
+                
+                
+
+
+
+
         }
     });
 });
